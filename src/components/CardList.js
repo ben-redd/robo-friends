de63214d5761037adef08dfbc7df102ef07db7c1
Ben Redd <bentheredd13@gmail.com>
2020-07-29 08:33:52 -0700

    initial commit

---
+++ src/components/CardList.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+	return (
+		<Fragment>
+			{robots.map((user, i) => {
+				return <Card key={i} id={user.id} name={user.name} email={user.email} />;
+			})}
+		</Fragment>
+	);
+};
+
+export default CardList;
