ce671839108d3ec3e941aa906d4082f7b238b26f
Ben Redd <bentheredd13@gmail.com>
2021-06-01 21:52:32 -0700

    finished converting my project to using redux for state handling/tracking

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { provider } from 'react-redux';
-import { createStore } from 'redux';
-import { searchRobots } from './reducers';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { searchRobots, requestRobots } from './reducers';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-const store = createStore(searchRobots);
+//creates logger that we get from the redux-logger package
+const logger = createLogger();
+
+//combines reducers into one rootReducer
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger),
+);
 
 ReactDOM.render(
-  <provider store={store}>
+  <Provider store={store}>
     <App store={store} />
-  </provider>,
+  </Provider>,
   document.getElementById('root'),
 );
 
